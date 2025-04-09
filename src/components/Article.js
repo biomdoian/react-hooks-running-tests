c3f42f2b4cb97714604567188ca4e2dd35749cb3
Ian <iankruze60@gmail.com>
2025-04-09 16:52:25 +0300

    Adds modified src/--components/Artivle.js and src/components/Header.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
+function Article(props) {
+  return <div>
+      please pass this test 
     </div>
-  );
 }
 
 export default Article;
+
